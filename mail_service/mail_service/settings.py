6344a6449bb884489ebae3dedebb34c98cfa7724
utkarsh <utkarshrathore6102@gmail.com>
2023-02-11 13:20:44 +0530

    update the database from default sqlite to postgres and added a .env.example file

--- mail_service/mail_service/settings.py
+++ mail_service/mail_service/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'mail_service.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
