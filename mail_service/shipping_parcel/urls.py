e9e0b259046a1b7d4e6dc4a6c4a20b7a3d7e4541
Utkarsh Rathore <54301690+utkarshrathore2000@users.noreply.github.com>
2023-02-11 21:29:53 +0530

    added create, list, withdrawl and train detail api endpoint (#12)

--- mail_service/shipping_parcel/urls.py
+++ mail_service/shipping_parcel/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import ParcelView, WithDrawParcel, ParcelShippedDetailView
+from .views import (ParcelShippedDetailView, ParcelView, PostTrainOfferView,
+                    TrainShippedDetailView, WithDrawParcel,
+                    WithDrawTrainOfferView)
 
 urlpatterns = [
     # Parcel related endpoints
     path("post-parcel/", ParcelView.as_view()),
     path("withdraw-parcel/<int:pk>/", WithDrawParcel.as_view()),
     path("parcel-shipped-detail/<int:pk>/", ParcelShippedDetailView.as_view()),
+    # Train post offer related endpoints
+    path("post-train-offer/", PostTrainOfferView.as_view()),
+    path("withdraw-train-offer/<int:pk>/", WithDrawTrainOfferView.as_view()),
+    path("train-shipped-detail/<int:pk>/", TrainShippedDetailView.as_view()),
 ]
